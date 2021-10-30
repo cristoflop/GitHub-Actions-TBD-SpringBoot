956a855c3ac8b4a56080e5ece0710f10a278b9f0
cristoflop <cristofer.lc.99@gmail.com>
2021-10-30 17:28:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Integración y Entrega continua - GitHub Actions
+
+Repositorio: [GitHub Actions](https://github.com/cristoflop/GitHub-Actions-TBD-SpringBoot.git)
+
+#### Autores:
+- [@cristoflop](https://github.com/cristoflop)
+- [@juanaviladev](https://github.com/juanaviladev)
---
+++ src/test/java/es/urjc/code/daw/library/e2e/selenium/SeleniumTest.java
@@ -0,0 +1,136 @@
+package es.urjc.code.daw.library.e2e.selenium;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import es.urjc.code.daw.library.Application;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SeleniumTest {
+
+    @LocalServerPort
+    int port;
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+	@BeforeAll
+	public static void setupClass() {
+		WebDriverManager.chromedriver().setup();
+	}
+
+	@BeforeEach
+	public void setupTest() {
+        port = Integer.parseInt(System.getProperty("port", port+""));
+        ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+        this.driver = new ChromeDriver(options);
+        this.wait = new WebDriverWait(driver, 10);
+	}
+
+	@AfterEach
+	public void teardown() {
+		if (this.driver != null) {
+            this.driver.quit();
+		}
+    }
+    
+    @Test
+    @DisplayName("Añadir un nuevo libro y comprobar que se ha creado")
+	public void createBookTest() throws Exception {
+
+        // GIVEN: Partiendo de que estamos en la página principal de la libreria
+        this.driver.get("http://localhost:"+this.port+"/");
+
+        // WHEN: Creamos un nuevo libro
+
+        String title = "FAKE BOOK";
+        String description = "Contenido de prueba";
+
+        this.createNewBook(title, description);
+
+        // THEN: Esperamos que el libro creado aparezca en la nueva página resultante
+
+        wait.until(ExpectedConditions.textToBe(By.id("bookTitle"), title));
+        wait.until(ExpectedConditions.textToBe(By.id("bookDescription"), description));
+    }
+
+    @Test
+	@DisplayName("Borrar un libro y comprobar que no existe")
+	public void deleteBookTest() throws Exception {
+
+        // GIVEN: Partiendo de que estamos en la página principal de la libreria
+        this.driver.get("http://localhost:"+this.port+"/");
+
+        // WHEN: 
+        
+        // Creamos un nuevo libro
+
+        String title = "FAKE BOOK TO DELETE";
+        String description = "Contenido de prueba 2";
+
+        this.createNewBook(title, description);
+
+        // Y lo borramos
+        driver.findElement(By.xpath("//*[text()='Remove']")).click();
+
+        // THEN: Comprobamos que el libro no existe
+
+        assertThrows(NoSuchElementException.class,()-> {
+            driver.findElement(By.linkText(title));
+        });
+
+    }
+
+    @Test
+    @DisplayName("Comprobar que el boton 'Cancel' en la página de edit devuelve a la página del libro")
+	public void cancelEditBookTest() throws Exception {
+
+        // GIVEN: Partiendo de que estamos en la página principal de la libreria
+        this.driver.get("http://localhost:"+this.port+"/");
+
+        // WHEN: Creamos un nuevo libro
+
+        String title = "FAKE BOOK TO DELETE";
+        String description = "Contenido de prueba 3";
+
+        this.createNewBook(title, description);
+
+        // WHEN: Editamos el libro
+
+        // Hacemos click en "New book"
+        driver.findElement(By.xpath("//*[text()='Edit']")).click();
+        
+        driver.findElement(By.xpath("//*[text()='Cancel']")).click();
+        // Navegamos a la página de todos los libros
+
+        // THEN: Esperamos estar en la página del libro
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("bookDescription")));
+    }
+
+    private void createNewBook(String title, String description){
+        // Hacemos click en "New book"
+        driver.findElement(By.xpath("//*[text()='New book']")).click();
+        // Rellenamos el formulario
+        driver.findElement(By.name("title")).sendKeys(title);
+        driver.findElement(By.name("description")).sendKeys(description );
+        // Enviamos el formulario
+        driver.findElement(By.id("Save")).click();
+    }
+    
+}
